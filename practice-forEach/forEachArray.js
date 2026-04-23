2b32c48d6359febef1f684cfbd0395d6743dc7bf
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-04-23 23:15:33 +0530

    map function added

--- practice-forEach/forEachArray.js
+++ practice-forEach/forEachArray.js
@@ -9,8 +9,19 @@ class MyArray {
             callback(this.array[i], i, this.array);
         }
     }
+    myMap(callback) {
+        let newArray = [];
+        for (let i = 0; i < this.array.length; i++) {
+            newArray.push(callback(this.array[i], i, this.array));
+        }
+        return newArray;
+    }
 }
 const arr = new MyArray([1, 2, 3]);
 arr.myForEach((element) => {
     console.log(element * 2);
 });
+let newArr = arr.myMap((element) => {
+    return element * 2;
+});
+console.log(newArr);
--- practice-forEach/forEachArray.ts
+++ practice-forEach/forEachArray.ts
@@ -1,13 +1,23 @@
+type Callback<T> = (element: T, index?: number, array?: T[]) => void
+
 class MyArray<T>{
-    array;
+    array: T[];
     constructor(array: T[]) {
         this.array = array;
     }
 
-    myForEach(callback: any): void {
+    myForEach(callback: Callback<T>): void {
         for(let i=0;i<this.array.length;i++) {
             callback(this.array[i], i, this.array);
         }
+    }
+
+    myMap(callback: Callback<T>) : Array<T> {
+        let newArray: T[] = [];
+        for(let i = 0; i< this.array.length; i++) {
+            newArray.push(callback(this.array[i], i, this.array) as T)
+        }
+        return newArray;
     }
 }
 
@@ -15,3 +25,9 @@ const arr = new MyArray([1, 2, 3]);
 arr.myForEach((element: any)=> {
     console.log(element*2);
 })
+
+let newArr = arr.myMap((element) => {
+    return element*2;
+})
+console.log(newArr)
+
