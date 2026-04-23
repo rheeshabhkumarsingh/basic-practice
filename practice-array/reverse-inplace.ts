843c5508baf143275234b39c5d31d2e2de20f7b4
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-04-24 01:03:52 +0530

    inline reverse array

---
+++ practice-array/reverse-inplace.ts
@@ -0,0 +1,36 @@
+/*
+Reverse Array
+
+Problem:
+Reverse the given array in-place.
+*/
+
+//wrong solution
+
+// export function reverse<T>(arr: Array<T>): Array<T> {
+//     if(!arr) {
+//         return arr;
+//     }
+//     for(let i=0;i<arr.length;i++) {
+//         const temp = arr.shift() as T;
+//         arr.push(temp);
+//     }
+//     return arr;
+// }
+
+let array = [1,2,3,4];
+function swap<T>(i: number, j: number, arr: T[]) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+let i =0;
+let j = array.length -1;
+while(j>i) {
+    swap(i, j, array);
+    i++;
+    j--;
+}
+console.log(array)
+
