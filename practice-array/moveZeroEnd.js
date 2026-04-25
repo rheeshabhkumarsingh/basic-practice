2682fe1196b83b2096ba8b9e6f44990c14a8910e
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-04-25 23:37:35 +0530

    move zero end initital

---
+++ practice-array/moveZeroEnd.js
@@ -0,0 +1,27 @@
+"use strict";
+/*
+Problem:
+Move all 0s to the end while maintaining order of non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+*/
+function moveZero(arr) {
+    let i = 0;
+    let j = arr.length - 1;
+    while (i < j) {
+        if (arr[i] === 0) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            i++;
+            j--;
+        }
+        else {
+            i++;
+        }
+    }
+    console.log(arr);
+    return arr;
+}
+moveZero([0, 1, 0, 3, 12]);
