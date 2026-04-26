c96fc6f2a629d150d1bb5dd7ec6adb47a38dbfb0
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-04-27 01:21:00 +0530

    added chat gpt solution

--- practice-array/plus-one.ts
+++ practice-array/plus-one.ts
@@ -11,4 +11,20 @@ console.log(plusOne([1,2,9]))
 
 console.log(plusOne([9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9]))
 
-//above solution will work for small numbers but will break for big numbers
+//above solution will work for small numbers but will break for big numbers like 100 digits
+//chat gpt solution
+function plusOneNew(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0; // carry over
+    }
+
+    // if all digits were 9
+    digits.unshift(1);
+    return digits;
+}
+
